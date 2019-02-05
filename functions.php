d3952710af822c2e10437fe4d83ccd121a826753
Ladoui Abdellah <ladoui@gmail.com>
2019-02-05 10:24:43 +0100

    added wordpress php file and structure

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+add_action('after_setup_theme', function () {
+  // Add post thumbnails support.
+  add_theme_support('post-thumbnails');
+  // Add title tag theme support.
+  add_theme_support('title-tag');
+  // Add HTML5 theme support.
+  add_theme_support('html5', [
+    'caption',
+    'comment-form',
+    'comment-list',
+    'gallery',
+    'search-form',
+    'widgets',
+  ]);
+  // Register navigation menus.
+  register_nav_menus([
+    'navigation' => __('Navigation', 'wordplate'),
+  ]);
+});
+
+// Enqueue and register scripts.
+add_action('wp_enqueue_scripts', function () {
+  wp_deregister_script('jquery');
+
+  wp_enqueue_style("$theme-style", get_template_directory_uri() . '/dist/main.css', [], date('d, m, Y'), 'all');
+
+  wp_enqueue_script("$theme-script", get_template_directory_uri() . '/dist/bundle.js', [], date('d, m, Y'), true);
+});
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+  <header>
+    <nav role="navigation">
+      <?php wp_nav_menu(['theme_location' => 'primary menu']); ?>
+    </nav>
+  </header>
